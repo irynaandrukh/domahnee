afb1974a5ce98b4594c058ba2e57a2e7299ba762
iryna andrukh <igorirka.1@mail.com>
2022-10-24 23:32:06 +0300

    Initial commit

---
+++ src/Doma.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Doma {
+    public static void main(String[] args) {
+
+  // Basic Java
+  // recKrkwpI9hx61imR
+
+  // First level: Level1 (решается через for)
+  // Вводится n. Найдите сумму такого выражения: 1+1/2+1/3+1/4…..+1/n
+        Scanner scanner = new Scanner(System.in);
+     int n = scanner.nextInt();
+     for (int i = 1+1/2+1/3+1/4 ; i < n; i++){
+         System.out.println(i );
+
+     }
+
+  // Level2 (решается через while)
+  // Вводится число (число может быть и маленьким и большим), но вмешается в int. Найти сумму всех цифр этого числа
+      //int n = scanner.nextInt();
+      //for (int i = Integer.MIN_VALUE; i < Integer.MAX_VALUE; i++) {
+      //    System.out.println(n);
+
+        }
+
+    }
+
+
